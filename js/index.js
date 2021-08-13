b574100eb0b3ef6c12d19b59ef197e9eeb2fe675
Scott Park <scottpark44@gmail.com>
2021-08-13 13:14:41 -0700

    Added Close Button to Hamburger Menu

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 let hamburger_menu = document.getElementById("hamburger_button")
 let nav_bar = document.getElementById("navigation")
+let hamburger_image = document.getElementById("hamburger_image")
 nav_bar.style.display = "none"
 
 hamburger_menu.onclick = function openMenu(){
     if(nav_bar.style.display == "none"){
         nav_bar.style.display = "flex"
+        hamburger_image.setAttribute("src","./images/xicon.png")
+        hamburger_image.setAttribute("style", "width: 40px; height: 40px;")
     }
     else{
         nav_bar.style.display = "none"
+        hamburger_image.setAttribute("src","./images/hamgurger_white-01-01.png")
+        hamburger_image.setAttribute("style", "height: 75px; width: auto;")
     }
 }
 
