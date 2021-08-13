60f3cb621b90139541f1f2e105ef78b4b4774932
Scott Park <scottpark44@gmail.com>
2021-08-13 12:42:05 -0700

    Made some changes to the files for Hamburger Menu

--- js/index.js
+++ js/index.js
@@ -2,14 +2,18 @@ let hamburger_menu = document.getElementById("hamburger_button")
 let nav_bar = document.getElementById("navigation")
 nav_bar.style.display = "none"
 
-
 hamburger_menu.onclick = function openMenu(){
     if(nav_bar.style.display == "none"){
-        console.log("HERE IT IS")
         nav_bar.style.display = "flex"
     }
     else{
-        console.log("GOIN BACK")
+        nav_bar.style.display = "none"
+    }
+}
+
+if(nav_bar.style.display != "none"){
+    document.getElementById("body").onclick = function closeMenu(){
+        console.log("WHY")
         nav_bar.style.display = "none"
     }
 }
