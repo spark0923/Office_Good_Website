9c1354ebfff1319203247853d81118cd157ab2a7
Scott Park <scottpark44@gmail.com>
2021-08-13 12:11:20 -0700

    Hamburger Menu Fix

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 let hamburger_menu = document.getElementById("hamburger_button")
 let nav_bar = document.getElementById("navigation")
+nav_bar.style.display = "none"
+
+
 hamburger_menu.onclick = function openMenu(){
-    if(nav_bar.style.display === "none"){
+    if(nav_bar.style.display == "none"){
         console.log("HERE IT IS")
         nav_bar.style.display = "flex"
     }
